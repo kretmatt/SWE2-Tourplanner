6854cd9f7934b65ee02517db9f79fc6c4ce54f40
kretmatt <mrtthias@gmail.com>
2021-03-07 21:00:25 +0100

    Add search prototype for tours(strings)

---
+++ SWE2-Tourplanner/SWE2-Tourplanner/Commands/SearchToursCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SWE2_Tourplanner
+{
+    public class SearchToursCommand : ICommand
+    {
+        private readonly MainViewModel _mainViewModel;
+
+        public SearchToursCommand(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+            _mainViewModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Tours" || args.PropertyName == "SearchFilter")
+                {
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            };
+
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _mainViewModel.Tours.Any() && !string.IsNullOrWhiteSpace(_mainViewModel.SearchFilter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _mainViewModel.FilteredTours = _mainViewModel.Tours.FindAll(t=>t.Contains(_mainViewModel.SearchFilter)==true);
+        }
+    }
+}
---
+++ SWE2-Tourplanner/SWE2-Tourplanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SWE2_Tourplanner
+{
+    public class MainViewModel:INotifyPropertyChanged
+    {
+        private string _searchFilter;
+
+        private List<string> _tours;
+        private List<string> _filteredTours;
+        public List<string> Tours
+        {
+            get
+            {
+                return _tours;
+            }
+            set
+            {
+                if (value != _tours)
+                {
+                    _tours = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public List<string> FilteredTours
+        {
+            get
+            {
+                return _filteredTours;
+            }
+
+            set
+            {
+                if(value!=_filteredTours)
+                {
+                    _filteredTours = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string SearchFilter
+        {
+            get
+            {
+                return _searchFilter;
+            }
+
+            set
+            {
+                if (value != _searchFilter)
+                {
+                    _searchFilter = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand SearchToursCommand { get; }
+
+        public MainViewModel()
+        {
+            SearchToursCommand = new SearchToursCommand(this);
+            Tours = new List<string> { "Tour 1", "Tour 2", "Tour 3", "Tour 34" };
+            FilteredTours = Tours;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
