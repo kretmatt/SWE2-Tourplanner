5b22e348305692e2ff92a0092aff4d68e863ffee
kretmatt <mrtthias@gmail.com>
2021-05-14 12:07:27 +0200

    Add interface and class for generation of tourplanner reports

---
+++ SWE2-Tourplanner/BusinessLogicLayer/PDFCreation/ITourPlannerReportsGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.PDFCreation
+{
+    public interface ITourPlannerReportsGenerator
+    {
+        Task GenerateTourReport(Tour tour);
+        Task GenerateSummaryReport(List<Tour> tours);
+    }
+}
